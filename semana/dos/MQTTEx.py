ba824e47707877392ce1c108118aa2230253d411
Nemo1710 <karlos1710@gmail.com>
2018-03-10 13:36:14 -0500

    Create MQTTEx.py

---
+++ semana/dos/MQTTEx.py
@@ -0,0 +1,36 @@
+
+
+import paho.mqtt.client as mqtt
+
+
+def on_message_msgs(mosq, obj, msg):
+    # This callback will only be called for messages with topics that match
+    # $SYS/broker/messages/#
+    print("MESSAGES: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+def on_message_bytes(mosq, obj, msg):
+    # This callback will only be called for messages with topics that match
+    # $SYS/broker/bytes/#
+    print("BYTES: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+def on_message(mosq, obj, msg):
+    # This callback will be called for messages that we receive that do not
+    # match any patterns defined in topic specific callbacks, i.e. in this case
+    # those messages that do not have topics $SYS/broker/messages/# nor
+    # $SYS/broker/bytes/#
+    
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+mqttc = mqtt.Client()
+
+# Add message callbacks that will only trigger on a specific subscription match.
+mqttc.message_callback_add("#", on_message_msgs)
+mqttc.message_callback_add("#", on_message_bytes)
+mqttc.on_message = on_message
+mqttc.connect("159.203.139.127", 1883, 60)
+mqttc.subscribe("#", 0)
+
+mqttc.loop_forever()
